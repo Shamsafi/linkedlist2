c74d74e7e2d9b1818cfc34606235d23065db916c
Safi <mdsafi729@gmail.com>
2023-05-02 09:28:52 +0530

    [Shamssuddin]Fix:Pushing UC4

---
+++ UC4.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Stack_Q_LL
+//{
+//    public class Node
+//    {
+//        public int data;
+//        public Node next;
+//        public Node(int data)
+//        {
+//            this.data = data;
+//        }
+//    }
+//    public class LLInsert
+//    {
+//        Node head;
+//        public void insert(int data)  // 56 30 70
+//        {
+//            Node n = new Node(data);
+//            if (head == null)
+//            {
+//                head = n;
+//            }
+//            else
+//            {
+//                n.next = head;
+//                head = n;
+//            }
+//        }
+
+//        public void display()
+//        {
+//            Node temp = head;
+//            while (temp != null)
+//            {
+//                Console.Write(temp.data + " ");
+//                temp = temp.next;
+//            }
+//        }
+
+//        static void Main()
+//        {
+//            LLInsert list = new LLInsert();
+//            list.insert(70);
+//            list.insert(30);
+//            list.insert(56);
+//            list.display();
+
+
+//        }
+//    }
+//}
