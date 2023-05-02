74da8a631dd06f90e2e241c3a279c7976e9d277f
Safi <mdsafi729@gmail.com>
2023-05-02 08:25:12 +0530

    [Shamssuddin]Fix:Pushing UC1

---
+++ UC1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Linked_List
+{
+    public class Program
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        public class Linked_List
+        {
+            private Node head;
+
+            public bool Add(int data)
+            {
+                Node n = new Node(data);
+                if (head == null)
+                {
+                    head = n;
+                    return true;
+                }
+                n.next = head;
+                head = n;
+                return true;
+            }
+
+            public void Print()
+            {
+                Node t = head;
+                while (t != null)
+                {
+                    Console.WriteLine(t.data);
+                    t = t.next;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Linked_List list = new Linked_List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Print();
+            Console.ReadKey();
+        }
+    }
+}
